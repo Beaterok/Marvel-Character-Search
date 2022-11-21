7c0d2d15f7b9a74e0034f93011e0c060b0a78bd7
bmfoster5 <foster.brandon96@gmail.com>
2022-11-21 18:49:46 -0500

    script push 1

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,14 @@ var searchBox = document.querySelector('example').value;
 var vidLink = document.querySelector('videolink');
 var searchBtn = document.getElementById('#searchBtn');
 // create variables
-var searchButton = document.querySelector('')
 
+
+// create functions for page load
 
 // create a funciton to retrieve API
-function getWebAPI() {
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
     var requestURL = "https://www.youtube.com/watch?v=S60GCTqsdI4";
 
     fetch(requestURL)
@@ -17,27 +20,34 @@ function getWebAPI() {
         .then(function (data) {
             console.log(data)
 
-    for (var i = 0; i < data.length; i++) {
-        var createColumn = document.createElement('col');
-        var dataTable = document.createElement('dt');
-        var link = document.createElement('a');
+            for (var i = 0; i < data.length; i++) {
+                var createColumn = document.createElement('col');
+                var dataTable = document.createElement('dt');
+                var link = document.createElement('a');
 
-        link.textContent = data[i].html_url;
-        link.href = data[i].html_url;
+                link.textContent = data[i].html_url;
+                link.href = data[i].html_url;
 
-        createColumn.appendChild(dataTable);
-        dataTable.appendChild(link);
-        searchBox.appendChild(createColumn);
-    }
+                createColumn.appendChild(dataTable);
+                dataTable.appendChild(link);
+                searchBox.appendChild(createColumn);
+            }
 
-    searchBtn.addEventListener('click', getAPI);
-// create functions for page load
+            searchBtn.addEventListener('click', getAPI);
 
-// create a function for return search input
+            // create a function for return search input
 
-// create function for button interaction
-searchBtn.addEventListene('submit', function(event) {
-    if (mode)
-}
+            // create function for button interaction
+            searchBtn.addEventListener('submit', function (event) {
+                event.preventDefault();
+                fetch
+
+            }
 
 // create function for video directory 
+
+// create a function to display tweets onto HTML
+
+// create a function to display character bio onto browser
+
+//
